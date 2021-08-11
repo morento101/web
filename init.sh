ca40461ff0de081825e591f3d2a4c4b99e471efd
morento101 <tlcees@gmail.com>
2021-08-11 16:54:47 +0300

    correct init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx/nginx.conf
+sudo ﻿ls -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
