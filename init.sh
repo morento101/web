f61db4d50e3179891648a17288275cfec54da55b
morento101 <tlcees@gmail.com>
2021-08-11 17:02:02 +0300

    correct nginx.conf

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ﻿ls -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx/nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
