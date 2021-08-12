f177653158de65d5e729f0ed60b402040d261ebf
morento101 <tlcees@gmail.com>
2021-08-12 17:51:54 +0300

    again hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 def app(env, start_response):
 	status = '200 OK'
 	data = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
-	response_headers = [
-		('Content-Type', 'text/plain'), 
-		('Content-Lenght', str(len(data)))
+	headres = [
+		('Content-Type', 'text/plain')
 	]
-	start_response(status, response_headers)
-	return iter(data)
+	start_response(status, headers)
+	return iter([data])
