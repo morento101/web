eec556b541874bd90c0eba1d1e0af8b784513629
morento101 <tlcees@gmail.com>
2021-08-13 11:33:48 +0300

    copied from other git

--- etc/hello.py
+++ etc/hello.py
@@ -1,15 +1,3 @@
 pythonpath = '/home/box/web/'  # путь до приложения
 bind = "0.0.0.0:8080"
 workers = 4
-
-#CONFIG = {
-#	'mode': 'wsgi',
-#	'working_dir': '/home/box/web',
-#	'python': 'usr/bin/python',
-#	'args': (
-#		'--bind=0.0.0.0:8080',
-#		'--workers=16',
-#		'--timeout=60',
-#		'hello:app'
-#	),
-#}
