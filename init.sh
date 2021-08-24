71352c26c6d06d03b65ffe57e71321ad86944c5d
morento101 <tlcees@gmail.com>
2021-08-24 15:36:18 +0300

    django

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
+sudo gunicorn -c /home/box/web/etc/django.conf ask.wsgi:application
 sudo /etc/init.d/nginx restart
