b9642aac3079e681ca61d8c3df79d034ebcccc0e
morento101 <tlcees@gmail.com>
2021-08-25 18:44:07 +0300

    (((((

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'stepic_web',
-        'USER': 'box'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
