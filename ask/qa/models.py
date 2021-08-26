4b971fbafacae49e8fba8d5d18c8da38943d8c1d
morento101 <tlcees@gmail.com>
2021-08-26 18:22:02 +0300

    manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+
+	def popular(self):
+		return self.order_by('-rating')
+
+
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
@@ -8,14 +16,7 @@ class Question(models.Model):
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User, related_name='question_like_user')
-
-
-class QuestionManager(models.Manager):
-	def new(self):
-		return self.order_by('-added_at')
-
-	def popular(self):
-		return self.order_by('-rating')
+	objects = QuestionManager()
 
 
 class Answer(models.Model):
