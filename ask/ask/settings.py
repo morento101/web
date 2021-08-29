893b669eaf23e162fbaba9d0ac74f088ae2e4f5e
morento101 <tlcees@gmail.com>
2021-08-29 21:49:39 +0300

    template dir

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ask.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r"os.path.join(BASE_DIR, 'qa/templates')"],
+        'DIRS': [r"os.path.join(BASE_DIR, 'templates')"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
