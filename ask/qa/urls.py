2fd64adcbb16658063d41b3269e318639683af82
morento101 <tlcees@gmail.com>
2021-08-29 18:30:05 +0300

    urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .views import question, popular, new_ques
 
 
 urlpatterns = [
-	url(r'^$', views.test),
+	url(r'^$', new_ques),
+	url(r'^question/(?P<id>[0-9]+)/', question),
+	url(r'popular/', popular),
 ]
--- sql.sh
+++ sql.sh
@@ -1,7 +1,5 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE stepic_web;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
-pip install django-autofixture
-python ask/manage.py loadtestdata qa.Question:20 qa.Answer:50
 python ask/manage.py makemigrations qa
 python ask/manage.py migrate
